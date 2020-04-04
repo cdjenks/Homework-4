65b67a0d3b39c7cd3c2496c2829d883bf3c97036
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-04-04 15:26:24 -0400

    timer and local storage code implemented; questions changed to math problems

--- script.js
+++ script.js
@@ -1,155 +1,297 @@
-var startButton = document.getElementById("start-button")
-var questionContainerButtons = document.getElementById("questions-container")
-var questionEl = document.getElementById("question")
-var answerButtonsEl = document.getElementById("answer-buttons")
-var answerResultField = document.getElementById("answer-feedback")
+var startButton = document.getElementById("start-button");
+var questionContainerButtons = document.getElementById("questions-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answer-buttons");
+var answerResultField = document.getElementById("answer-feedback");
+var savedScoresButton = document.getElementById("saved-scores")
 
+var randomQuestions, currentQuestionIndex;
 
-var randomQuestions, currentQuestionIndex
+var quizGameTimer = document.getElementById("quiz-timer");
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var secondsLeft = 90;
 
+function quizTimer() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    quizGameTimer.innerText = secondsLeft + " seconds remaining";
 
+    if (secondsLeft <= 0 || randomQuestions.length <= currentQuestionIndex + 1) {
+      clearInterval(timerInterval);
+      enterScore();
+    }
+  }, 1000);
+}
 
-startButton.addEventListener("click", startGame)
+savedScoresButton.addEventListener("click", viewScores);
+startButton.addEventListener("click", startGame);
 
-
+function viewScores() {
+    alert(localStorage.getItem("Scores"));
+}
 
 function startGame() {
-    startButton.classList.add("hide")
-    randomQuestions = quizQuestions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerButtons.classList.remove("hide")   
-    setNextQuestion()
+  startButton.classList.add("hide");
+  randomQuestions = quizQuestions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerButtons.classList.remove("hide");
+  setNextQuestion();
+  quizTimer();
 }
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(randomQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(randomQuestions[currentQuestionIndex]);
 }
 
-
-
 function showQuestion(question) {
-    questionEl.innerText = question.question
-    question.answers.forEach(answer => {
-        var button = document.createElement("button")
-        button.innerText = answer.text
-        button.classList.add("button")
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener("click", selectAnswer)
-        answerButtonsEl.appendChild(button)
-    })
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    var button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("button");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsEl.appendChild(button);
+  });
 }
 
 function resetState() {
-    while (answerButtonsEl.firstChild) {
-        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-
-    }
+  while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  }
 }
 
 function selectAnswer(e) {
-    var selectedAnswer = e.target
-    var correct = selectedAnswer.dataset.correct
-    if (correct) {
-        answerResultField.innerText = "Correct!"
-        answerResultField.classList.remove("hide")
-        quizStatusCheck()
-    } else {
-        answerResultField.innerText = "Incorrect :("
-        answerResultField.classList.remove("hide")
-        quizStatusCheck()
-    }
+  var selectedAnswer = e.target;
+  var correct = selectedAnswer.dataset.correct;
+  if (correct) {
+    answerResultField.classList.remove("hide");
+    answerResultField.innerText = "Correct!";
+    correctAnswers++;
+    quizStatusCheck();
+  } else {
+    answerResultField.classList.remove("hide");
+    answerResultField.innerText = "Incorrect :(";  
+    secondsLeft -= 5;
+    incorrectAnswers++;
+    quizStatusCheck();
+  }
 }
-
 
 function quizStatusCheck() {
-if (randomQuestions.length > currentQuestionIndex + 1){
-    currentQuestionIndex++
-    setNextQuestion()
-    } else 
-    enterScore()
+  if (randomQuestions.length > currentQuestionIndex + 1) {
+    currentQuestionIndex++;
+    setNextQuestion();
+  } else enterScore();
 }
-
 
 function enterScore() {
-
+  var userInitials = prompt(
+    `Correct: ${correctAnswers}\nIncorrect: ${incorrectAnswers}\nPlease enter your initials`
+  );
+  var scores = getLocalScores();
+  var gameResults = {Correct: correctAnswers,Incorrect: incorrectAnswers, Initials: userInitials};
+  scores.push(gameResults)
+  localStorage.setItem("Scores", JSON.stringify(scores))
 }
 
-
+function getLocalScores() {
+    var storedScores = localStorage.getItem("Scores");
+    if  (storedScores && storedScores[0] == "{"){
+        let scoresArray = [];
+        scoresArray.push(storedScores)
+        return scoresArray
+    } else if  (storedScores && storedScores[0] == "["){
+        return JSON.parse(storedScores)
+    } else {
+        return []
+    }
+}
 
 var quizQuestions = [
-    {
-        question: "In the year 1900 in the U.S. what were the most popular first names given to boy and girl babies?",
+  {
+      question:
+        "What is 18 squared?",
         answers: [
-            {text: "William and Elizabeth", correct: false },
-            {text: "Joseph and Catherine", correct: false },
-            {text: "John and Mary", correct: true },
-            {text: "George and Anne", correct: false }
-        ]
-    },
-    {
-        question: "When did the Libery Bell get its name?",
+        {text: "256", correct: false},
+        {text: "92", correct: false},
+        {text: "644", correct: false},
+        {text: "324", correct: true},
+    ],
+  },
+  {
+      question:
+        "What is 845 minus 138?",
         answers: [
-            {text: "When it was made, in 1701", correct: false },
-            {text: "when it rang on July 4th, 1776", correct: false },
-            {text: "In the 19th century, when it became a symbol of the abolition of slavery", correct: true },
-            {text: "None of the above", correct: false }
-        ]   
-    },
-    {
-        question: "Which of the following items was owned by the fewest U.S. homes in 1990?",
+        {text: "588", correct: false},
+        {text: "698", correct: false},
+        {text: "707", correct: true},
+        {text: "714", correct: false},
+      ],
+  },
+  {
+      question: 
+        "What is 4400 divided by 400?",
         answers: [
-            {text: "Home computer", correct: false},
-            {text: "Compact disk player", correct: true},
-            {text: "cordless phone", correct: false},
-            {text: "Dishwasher", correct: false}
-        ]
-    },
-    {
-        question: "In 1990, in what percentage of U.s. married couples did the wife earn more money than the husband?",
+        {text: "11", correct: true},
+        {text: "20", correct: false},
+        {text: "22", correct: false},
+        {text: "16", correct: false},
+      ],
+  },
+  {
+      question:
+        "What is 22 multiplied by 4.5?",
         answers: [
-            {text: "8", correct: false},
-            {text: "18", correct: true},
-            {text: "38", correct: false},
-            {text: "58", correct: false}
-        ]
-    },
-    {
-        question: "During the 1980s for six consecutive years what breed of dog was the most popular in the U.S.?",
+        {text: "103", correct: false},
+        {text: "88", correct: false},
+        {text: "95", correct: false},
+        {text: "99", correct: true},
+      ],
+  },
+  {
+      question:
+        "What is 9 multiplied by 84?",
         answers: [
-            {text: "Cocker spaniel", correct: true},
-            {text: "German shepherd", correct: false},
-            {text: "Labrador retriever", correct: false},
-            {text: "Poodle", correct: false}
-        ]
-    },
-    {
-        question: "The first black American pictured on a U.S. postage stamp was who?",
+        {text: "680", correct: false},
+        {text: "756", correct: true},
+        {text: "556", correct: false},
+        {text: "804", correct: false},
+        ],
+  },
+  {
+    question:
+        "What is 720 divided by 24?",
         answers: [
-            {text: "Frederick Douglass", correct: false},
-            {text: "Booker T Washington", correct: false},
-            {text: "Louis Armstrong", correct: false},
-            {text: "Joe Louis", correct: true}
-        ]
-    }
-]
-
-
-
-//     {
-//         question: "Which of the following items was owned by the fewst U.S. homes in 1990?",
-//         answers: {
-//             a: "home computer",
-//             b: "compact disk player",
-//             c: "cordless phone",
-//             d: "dishwasher",
-//         },
-//         correctAnswer: "b"
-//     },
-// ]
-
-
-
+        {text: "12", correct: false},
+        {text: "30", correct: true},
+        {text: "25", correct: false},
+        {text: "39", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 18(8 - 13)?",
+        answers: [
+        {text: "-90", correct: true},
+        {text: "-104", correct: false},
+        {text: "86", correct: false},
+        {text: "-62", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 7 cubed?",
+        answers: [
+        {text: "343", correct: true},
+        {text: "291", correct: false},
+        {text: "309", correct: false},
+        {text: "98", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What percentage of 200 is 120?",
+        answers: [
+        {text: "55%", correct: false},
+        {text: "60%", correct: true},
+        {text: "74%", correct: false},
+        {text: "70%", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 0.7 times 20?",
+        answers: [
+        {text: "9", correct: false},
+        {text: "13", correct: false},
+        {text: "16.2", correct: false},
+        {text: "14", correct: true},
+        ],  
+  },
+  {
+    question:
+        "What is 5 times 20.2?",
+        answers: [
+        {text: "148", correct: false},
+        {text: "104", correct: false},
+        {text: "110", correct: false},
+        {text: "101", correct: true},
+        ],  
+  },
+  {
+    question:
+        "What is the squareroot of 256?",
+        answers: [
+        {text: "17", correct: false},
+        {text: "28", correct: false},
+        {text: "16", correct: true},
+        {text: "22", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 14.9 - 9.3?",
+        answers: [
+        {text: "5.6", correct: true},
+        {text: "5.9", correct: false},
+        {text: "4.7", correct: false},
+        {text: "6.2", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 30(33 - 24)?",
+        answers: [
+        {text: "270", correct: true},
+        {text: "233", correct: false},
+        {text: "250", correct: false},
+        {text: "310", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 583 - 457?",
+        answers: [
+        {text: "154", correct: false},
+        {text: "126", correct: true},
+        {text: "95", correct: false},
+        {text: "104", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 44 + 37 + 121?",
+        answers: [
+        {text: "180", correct: false},
+        {text: "202", correct: true},
+        {text: "208", correct: false},
+        {text: "188", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is (50-39)(60 - 42)?",
+        answers: [
+        {text: "146", correct: false},
+        {text: "208", correct: false},
+        {text: "198", correct: true},
+        {text: "213", correct: false},
+        ],  
+  },
+  {
+    question:
+        "What is 4.5 times 12.2?",
+        answers: [
+        {text: "57", correct: false},
+        {text: "54.9", correct: true},
+        {text: "48.3", correct: false},
+        {text: "50.7", correct: false},
+        ],  
+  },
+];
 
